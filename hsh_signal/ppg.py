4831299f5546019fcf50cd7f160e07e3a5840da7
David Madl <git@abanbytes.eu>
2016-12-10 18:05:21 +0100

    remove beats too close to each other

--- hsh_signal/ppg.py
+++ hsh_signal/ppg.py
@@ -1,7 +1,16 @@
 from brueser.brueser2 import brueser_beatdetect
 from .heartseries import HeartSeries
+import numpy as np
 
 
 def ppg_beatdetect(ppg):
     ibeats, ibis = brueser_beatdetect(ppg.x, ppg.fps)
+
+    # remove beats too close to each other
+    deltas = np.diff(np.insert(np.array(ibeats), 0, 0) / float(ppg.fps))
+    idupes = np.where(deltas <= 0.3)[0]
+    ibeats = np.delete(ibeats, idupes)
+
+    ibeats = ibeats[np.arange(len(ibeats))]
+
     return HeartSeries(ppg.x, ibeats, fps=ppg.fps, lpad=ppg.lpad)
