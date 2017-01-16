5dbe3c07ae39f3b03f0b6ccbee36c6ab02262078
David Madl <git@abanbytes.eu>
2017-01-16 12:27:16 +0100

    AMFilter parameterize

--- hsh_signal/am.py
+++ hsh_signal/am.py
@@ -27,14 +27,16 @@ class _AMDemod(FilterBlock):
 
 class AMFilter(SourceBlock):
     """Demodulates an AM audio signal."""
-    def __init__(self, fps):
+    def __init__(self, fps, low_cutoff_freq, high_cutoff_freq, transition_width, min_freq=None, max_freq=None, loop_bw=None):
         super(AMFilter, self).__init__()
         self.sampling_rate = fps
 
-        f_carrier = 10000
+        if min_freq is None: min_freq = low_cutoff_freq
+        if max_freq is None: max_freq = high_cutoff_freq
+        if loop_bw is None: loop_bw = (max_freq - min_freq)
 
-        self.prefilter = Bandpass(low_cutoff_freq=f_carrier-100, high_cutoff_freq=f_carrier+100, transition_width=20, sampling_rate=self.sampling_rate)
-        self.demod = _AMDemod(loop_bw=100, max_freq=f_carrier+1000, min_freq=f_carrier-1000, sampling_rate=self.sampling_rate)
+        self.prefilter = Bandpass(low_cutoff_freq=low_cutoff_freq, high_cutoff_freq=high_cutoff_freq, transition_width=transition_width, sampling_rate=self.sampling_rate)
+        self.demod = _AMDemod(loop_bw=loop_bw, max_freq=max_freq, min_freq=min_freq, sampling_rate=self.sampling_rate)
         self.lowpass = Lowpass(cutoff_freq=100, transition_width=5, sampling_rate=self.sampling_rate)
 
         self.delay = self.prefilter.delay + self.demod.delay + self.lowpass.delay
