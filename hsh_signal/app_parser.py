4b4c9575d08648f41f5369f0a8d465540cca2abb
David Madl <git@abanbytes.eu>
2017-06-04 16:42:03 +0200

    various AppData goodies

--- hsh_signal/app_parser.py
+++ hsh_signal/app_parser.py
@@ -20,17 +20,17 @@ import json
 from .hsh_data import MyJSONEncoder
 
 
-def classify_results(meta_data, series_data):
+def classify_results(meta_data, series_data, host='https://mlapi.heartshield.net'):
     post_data = {
         'meta_data': json.dumps(meta_data, cls=MyJSONEncoder),
         'series_data': json.dumps(series_data, cls=MyJSONEncoder)
     }
-    response = requests.post('https://mlapi.heartshield.net/v2/reclassify', post_data)
+    response = requests.post(host + '/v2/reclassify', post_data)
     if response.status_code != 200:
         raise RuntimeError('requests.post() status code != 200: ' + response.text)
 
     result_dict = json.loads(response.text)
-    prob, filtered, idx = [result_dict[k] for k in 'pred filtered idx'.split()]
+    #prob, filtered, idx = [result_dict[k] for k in 'pred filtered idx'.split()]  # does not work for 'unclassified'
 
     #meta_fname = self.hs_data.put_result(meta_data, result_dict)
     return result_dict
@@ -82,11 +82,22 @@ def server_series_filename(meta_data):
     return '{}_{}_series.b'.format(unixtime, sane_app_id)
 
 
-def audio_filename(audio_base, meta_data):
+def audio1_filename(audio_base, meta_data):
     sane_app_id = sanitize(meta_data['app_info']['id'], '0123456789ABCDEF')
     start_time = int(calendar.timegm(meta_data['start_time'].utctimetuple()))-3600  # TODO: server timezone :(
     return os.path.join(audio_base, '{}_series.b_{}'.format(start_time, sane_app_id))
 
+def audio2_filename(audio_base, meta_data):
+    sane_app_id = sanitize(meta_data['app_info']['id'], '0123456789ABCDEF')
+    start_time = int(calendar.timegm(meta_data['start_time'].utctimetuple()))
+    return os.path.join(audio_base, '{}_{}'.format(start_time, sane_app_id))
+
+def audio_filename(audio_base, meta_data):
+    ans = audio1_filename(audio_base, meta_data), audio2_filename(audio_base, meta_data)
+    for an in ans:
+        if os.path.exists(an):
+            return an
+    return ans[0]
 
 class LazyDict(dict):
     """avoids unpickling a lot of data, unless we actually need it."""
@@ -214,7 +225,9 @@ class AppData:
             try:
                 raw_sig, fps = load_raw_audio(audio_filename(audio_base, self.meta_data))
                 retval = ecg_snr(raw_sig, fps) > THRESHOLD  # below, very few ECGs are usable...
-            except ValueError:
+            except (ValueError, EOFError):
+                # EOFError for 0-byte transmissions (from new Android client...)
+
                 # File "/mnt/hsh/hsh-signal/hsh_signal/alivecor.py", line 68, in load_raw_audio
                 # arr = arr.reshape((nframes, wf.getnchannels())).T
                 # ValueError: total size of new array must be unchanged
@@ -339,14 +352,20 @@ class AppData:
 
         return ecg, ppg, ecg_ibs, ppg_ibs
 
-    def get_result(self, reclassify=False):
+    def get_result(self, reclassify=False, host='https://mlapi.heartshield.net'):
         """calls HS API /reclassify if necessary (if result not yet cached).
         :returns result dict with keys ['pred', 'filtered', 'idx']"""
-        cache_file = os.path.join(AppData.CACHE_DIR, os.path.basename(self.meta_filename) + '_result.b')
-        if os.path.exists(cache_file) and not reclassify:
-            return np.load(cache_file)
+        cache_files = [
+            self.meta_filename.replace('_meta', '_result'),
+            os.path.join(AppData.CACHE_DIR, os.path.basename(self.meta_filename) + '_result.b')
+            ]
+        if not reclassify:
+            for cache_file in cache_files:
+                if os.path.exists(cache_file):
+                    return np.load(cache_file)
 
-        res = classify_results(self.meta_data, self.series_data)
+        self.series_data.load()
+        res = classify_results(self.meta_data, self.series_data, host=host)
 
         if os.path.isdir(AppData.CACHE_DIR):
             with open(cache_file, 'wb') as fo:
@@ -376,8 +395,14 @@ class AppData:
         doctor = self.meta_data['doctor']
         return doctor['text'].replace('\n', ' ')
 
+    def model(self):
+        return self.meta_data['app_info']['install_android_versions']['Build.MODEL'] if ('app_info' in self.meta_data and 'install_android_versions' in self.meta_data['app_info'] and 'Build.MODEL' in self.meta_data['app_info']['install_android_versions']) else None
+
     def app_id(self):
         return self.meta_data['app_info']['id']
+
+    def app_version(self):
+        return self.meta_data['app_info']['version'] if ('app_info' in self.meta_data and 'version' in self.meta_data['app_info']) else None
 
     def user_name(self):
         """returns empty string if unknown."""
@@ -414,7 +439,11 @@ class AppData:
         return None
 
     def _age_or_gender(self, ftype='age'):
-        # TODO: new app v0.3 parser
+        # new app v0.3 parser (line of apps which always ask for age/gender)
+        if 'user' in self.meta_data:
+            return self.meta_data['user'][ftype]
+
+        # old app series v0.1.*
         if not 'doctor' in self.meta_data:
             return None
         doctor = self.meta_data['doctor']
@@ -428,11 +457,9 @@ class AppData:
         return details[ftype]  # string!
 
     def age(self):
-        # TODO: new app v0.3 parser
         return self._age_or_gender('age')
 
     def gender(self):
-        # TODO: new app v0.3 parser
         return self._age_or_gender('gender')
 
     def get_cvd_status(self):
